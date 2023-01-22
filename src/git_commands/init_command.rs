72dc16c6bd9d8159de824a924e9b69f77d9facaf
hnakashima <nakashima.alg57@gmail.com>
2023-01-22 20:35:19 +0900

    test: fix a test

--- src/git_commands/init_command.rs
+++ src/git_commands/init_command.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 
 use super::GitCommand;
 
@@ -18,16 +18,20 @@ mod tests {
 
     #[test]
     fn test_init_command() {
-        let current_dir = std::env::current_dir()
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .to_owned();
-        let temp_path = format!("{}/temp", &current_dir);
+        let temp_path = format!("temp");
         std::env::set_current_dir(&temp_path).unwrap();
         let command = InitCommand {};
         let args: Vec<String> = Vec::new();
         command.execute(&args);
+        let paths = fs::read_dir(".git").unwrap();
+        for path in paths {
+            println!("Name: {}", path.unwrap().path().display())
+        }
+
+        assert!(std::path::Path::new(&format!(".git")).exists());
+        assert!(std::path::Path::new(&format!(".git/objects")).exists());
+        assert!(std::path::Path::new(&format!(".git/refs")).exists());
+        assert!(std::path::Path::new(&format!(".git/HEAD")).exists());
         std::fs::remove_dir_all(".git").expect("Failed to remove .git directory");
     }
 }
